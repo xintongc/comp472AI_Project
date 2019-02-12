c1acd89c689593124383feb5cbbdf94afcfea1b2
Shunyu Wang <shunyu878@gmail.com>
2019-02-11 23:20:58 -0500

    add card_type property to card

--- Card.py
+++ Card.py
@@ -1,6 +1,6 @@
 class Card:
-    def __init__(self, index_in_cardtype, half1, half2, card_id):
-        self.index_in_cardtype = index_in_cardtype
+    def __init__(self, card_type, half1, half2, card_id):
+        self.card_type = card_type
         self.half1 = half1
         self.half2 = half2
         self.card_id = card_id
