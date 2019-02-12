8e32ce75ceaf6abe6fd717784ef894793efb7299
Shunyu Wang <shunyu878@gmail.com>
2019-02-11 23:17:32 -0500

    add more example

--- Card.py
+++ Card.py
@@ -1,8 +1,9 @@
 class Card:
-    def __init__(self, id, half1, half2):
-        self.id = id
+    def __init__(self, index_in_cardtype, half1, half2, card_id):
+        self.index_in_cardtype = index_in_cardtype
         self.half1 = half1
         self.half2 = half2
+        self.card_id = card_id
 
     def name(self):
         return self.half1.name() + ' ' + self.half2.name()
