0102ab3408e6b16149b9fa11433f67b354fabaa3
Xintong Cheng <xcheng@mogiletech.com>
2019-02-13 22:00:52 -0500

    init card id to None

--- Card.py
+++ Card.py
@@ -1,11 +1,11 @@
 import copy
 
 class Card:
-    def __init__(self, card_type, half1, half2, card_id):
+    def __init__(self, card_type, half1, half2):
         self.card_type = card_type
         self.half1 = half1
         self.half2 = half2
-        self.card_id = card_id
+        self.card_id = None
 
     def name(self):
         return self.half1.name() + ' ' + self.half2.name()
